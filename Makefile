225ef9c11324becd01d9685f80bd809ad30bb54d
Diego Rubio <diegorubiomorato@gmail.com>
2024-05-01 14:27:03 +0200

    first structure created

---
+++ Makefile
@@ -0,0 +1,66 @@
+NAME = cub3d
+CC = gcc
+CFLAGS = -Wall -Werror -Wextra -I ./include -I ./libft/libft.h #-g3 -fsanitize=address
+
+RM = rm -f
+
+SRC_SRC = main.c
+PRS_SRC =
+GME_SRC =
+
+SRC = $(SRC_SRC) $(PRS_SRC) $(GME_SRC)
+
+INCLUDES = ./include/cub3d.h ./libft/libft.h
+LIBFT_DIR = libft/
+LIBFT = $(LIBFT_DIR)libft.a
+
+SRC_DIR = ./src/
+PRS_DIR = $(SRC_DIR)parser/
+GME_DIR = $(SRC_DIR)game/
+OBJ_DIR = ./obj/
+
+OBJ_FILES = $(SRC:.c=.o)
+OBJ = $(addprefix $(OBJ_DIR), $(OBJ_FILES))
+
+# RULES
+all: $(OBJ_DIR) $(LIBFT_DIR) $(NAME)
+
+$(LIBFT): $(LIBFT_DIR)
+	@make -C $(LIBFT_DIR)
+
+$(OBJ_DIR):
+	@mkdir -p $(OBJ_DIR)
+
+# Objects for Main
+$(OBJ_DIR)%.o: $(SRC_DIR)%.c
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+# Objects for Parser
+$(OBJ_DIR)%.o: $(PRS_DIR)%.c
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+# Objects for Game
+$(OBJ_DIR)%.o: $(GM_DIR)%.c
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+# basic library compiled
+$(NAME): $(OBJ) $(LIBFT)
+	@$(CC) $(CFLAGS) $(DEBUG) $(OBJ) $(LIBFT) -lm -o $(NAME)
+	@echo "$(GREEN)#### cub3d ####$(COLOR_OFF)"
+	@echo "    -Has been compiled ✅"
+
+# all .o files removed
+clean:
+	@$(RM) $(OBJ)
+	@make clean -C libft
+	@rm -rf $(OBJ_DIR)
+
+# library .a file removed
+fclean: clean
+	@$(RM) $(NAME)
+	@make fclean -C libft
+	@rm -rf $(OBJ_DIR)
+
+re:	fclean all
+
+.PHONY:	all clean fclean re
---
+++ include/cub3d.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 13:52:16 by drubio-m          #+#    #+#             */
+/*   Updated: 2024/05/01 14:21:40 by drubio-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+// System libraries
+# include <stdio.h>             /* Para printf */
+# include <readline/readline.h> 
+# include <readline/history.h>
+# include <signal.h>
+# include <stdlib.h>			
+# include <unistd.h>			
+# include <sys/wait.h>		    
+# include <signal.h>			
+# include <sys/stat.h>		   
+# include <fcntl.h>		    	
+# include <dirent.h>			
+# include <string.h>			
+# include <errno.h>			   
+# include <termios.h>		    
+# include <curses.h>			
+# include <limits.h>
+# include <math.h>
+
+// Our libraries
+# include "../libft/libft.h"
+# include "parser.h"
+# include "game.h"
+#endif

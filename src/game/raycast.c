8ed8412ddc7dd51ce1ce37d59e57141352c26e06
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2024-07-16 18:26:19 +0200

    refactoring raycasting

--- src/game/raycast.c
+++ src/game/raycast.c
@@ -6,11 +6,39 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:33:39 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/07/16 17:16:08 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2024/07/16 18:21:00 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
+
+int worldMap[24][24] =
+    {
+        {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 7, 7, 7, 7, 7, 7, 7},
+        {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+        {4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+        {4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+        {4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+        {4, 0, 4, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 0, 7, 7, 7, 7, 7},
+        {4, 0, 5, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+        {4, 0, 6, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
+        {4, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 1},
+        {4, 0, 8, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
+        {4, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+        {4, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 0, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 1},
+        {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+        {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+        {6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+        {4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 6, 0, 6, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3},
+        {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+        {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+        {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+        {4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2},
+        {4, 0, 0, 5, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+        {4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+        {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+        {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3}};
+
 
 void	init_raycast_variables(t_game *game, int x)
 {
@@ -18,21 +46,68 @@ void	init_raycast_variables(t_game *game, int x)
 
 	ray = game->ray;
 	ray->camX = (2 * x / (double)(SCREEN_X)) - 1;
-	ray->rayDirX = game->player->dirX + game->player->planeX * ray->camX;
-	ray->rayDirY = game->player->dirY + game->player->planeY * ray->camX;
+	ray->dirX = game->player->dirX + game->player->planeX * ray->camX;
+	ray->dirY = game->player->dirY + game->player->planeY * ray->camX;
 	ray->mapX = (int)(game->player->playerX);
 	ray->mapY = (int)(game->player->playerY);
-	ray->deltaDistX = fabs(1 / ray->rayDirX);
-	ray->deltaDistY = fabs(1 / ray->rayDirY);
+	ray->deltaDistX = fabs(1 / ray->dirX);
+	ray->deltaDistY = fabs(1 / ray->dirY);
+	ray->hit = 0;
 }
+
 
 void	looping_rays(t_game *game)
 {
 	t_ray *ray;
 
 	ray = game->ray;
-	if ()
+	if (ray->dirX < 0)
 	{
-		
+		ray->stepX = -1;
+		ray->sideDistX = (game->player->playerX - ray->mapX) * ray->deltaDistX;
 	}
+	else
+	{
+		ray->stepX = 1;
+		ray->sideDistX = (ray->mapX + 1.0 - game->player->playerX) * ray->deltaDistX;
+	}
+	if (ray->dirY < 0)
+	{
+		ray->stepY = -1;
+		ray->sideDistY = (game->player->playerY - ray->mapY) * ray->deltaDistY;
+	}
+	else
+	{
+		ray->stepY = -1;
+		ray->sideDistY = (ray->mapY + 1.0 - game->player->playerY) * ray->deltaDistY;
+	}
+	
+}
+
+
+void	wall_distance(t_game *game)
+{
+	t_ray *ray;
+
+	ray = game->ray;
+	while (ray->hit == 0)
+	{
+		if (ray->sideDistX < ray->sideDistY)
+		{
+			ray->sideDistX += ray->deltaDistX;
+			ray->mapX += ray->stepX;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->sideDistY += ray->deltaDistY;
+			ray->mapY += ray->stepY;
+			ray->side = 1;
+		}		
+		if (ray->side == 0)
+			ray->wallDist = ray->sideDistX - ray->deltaDistX;
+		else
+			ray->wallDist = ray->sideDistY - ray->deltaDistY;
+	}
+	
 }
